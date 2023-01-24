eec489d13f5e83e394949e25d88c8147e17205bb
EvgenyS <e-stolpovski@mail.ru>
2023-01-24 22:11:40 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from func import func_add_two_numbers
+from func import func_add_two_numbers, func_subtract_two_numbers
 
 print(func_add_two_numbers(3, 4))
+
+print(func_subtract_two_numbers(3, 4))
